86bfe6e98cd8cf974dbade730389f88df46b4365
karankumarece19 <karankumarece2025@liet.in>
2025-06-16 12:51:46 +0530

    updated resume

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -48,7 +48,7 @@ const About = () => {
           </p>
           {/* Resume Button */}
           <a
-            href="https://drive.google.com/file/d/1TzP-E6mn1As6Y9fa9qP_WHsJoDPvXHFO/view?usp=sharing"
+            href="https://drive.google.com/file/d/1rdeXB18eFexdlaNmWnHxQxaGDee-brfs/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
@@ -57,7 +57,7 @@ const About = () => {
               boxShadow: '0 0 2px #8245ec, 0 0 2px #8245ec, 0 0 40px #8245ec',
             }}
           >
-            DOWNLOAD CV
+            DOWNLOAD RESUME
           </a>
           
         </div>
